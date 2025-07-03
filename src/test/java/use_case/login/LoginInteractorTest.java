fd4a5e25d850d929ac7afc6e484ee7b9656ed2a8
Taabish Khan <vuvuzela@Anduril.local>
2025-07-02 20:46:19 -0400

    added @Test tag for successUserLoggedInTest

--- src/test/java/use_case/login/LoginInteractorTest.java
+++ src/test/java/use_case/login/LoginInteractorTest.java
@@ -40,7 +40,7 @@ public class LoginInteractorTest {
         LoginInputBoundary interactor = new LoginInteractor(userRepository, successPresenter);
         interactor.execute(inputData);
     }
-
+    @Test
     public void successUserLoggedInTest() {
         LoginInputData inputData = new LoginInputData("Paul", "password");
         LoginUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
